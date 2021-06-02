13d3b7393f46c8133156f1753b4fe23bb7913bd3
Dmitry Yermoshkin <spanb4@gmail.com>
2021-06-02 19:52:53 +0300

    feat: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Factions from './components/Factions/Factions';
 import Search from './components/Search/Search';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
     return (
         <div className="App">
-            <Search />
-            <Factions />
+            <Tabs>
+                <TabList>
+                    <Tab>Factions</Tab>
+                    <Tab>Search</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <Factions />
+                </TabPanel>
+                <TabPanel>
+                    <Search />
+                </TabPanel>
+            </Tabs>
         </div>
     );
 }
